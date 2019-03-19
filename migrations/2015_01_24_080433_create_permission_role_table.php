b991a7baa2ef42bc28c7c2d311ea99f964e07dd4
FGuillen <38765460+guillmz@users.noreply.github.com>
2019-03-19 09:56:06 -0700

    Update 2015_01_24_080433_create_permission_role_table.php

--- migrations/2015_01_24_080433_create_permission_role_table.php
+++ migrations/2015_01_24_080433_create_permission_role_table.php
@@ -14,9 +14,9 @@ class CreatePermissionRoleTable extends Migration
     {
         Schema::create('permission_role', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('permission_id')->unsigned()->index();
+            $table->bigInteger('permission_id')->unsigned()->index();
             $table->foreign('permission_id')->references('id')->on('permissions')->onDelete('cascade');
-            $table->integer('role_id')->unsigned()->index();
+            $table->bigInteger('role_id')->unsigned()->index();
             $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
             $table->timestamps();
         });
